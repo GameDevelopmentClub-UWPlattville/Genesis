65b98f9193a64b29cb92b83506c35f5d5c15faff
unknown <stewartjos@WKS003781.ion.uwplatt.edu>
2015-04-16 15:30:10 -0500

    Created the player movement script and a Scene to test it. -Josiah
    Updated the player movement and camera scripts so that the camera
    will follow the player smoothly and the movement controls are relative
    to the camera.  There is functionality to adjust the controllability
    of the ball depending on if the ball is on the ground or not.
    
    The test scene has a fun jump in it.  See if you can make the jump!

--- Scripts/RollingPlayerController.cs
+++ Scripts/RollingPlayerController.cs
@@ -1,41 +1,68 @@
-﻿using UnityEngine;
+﻿/* 
+ * The rolling Player Controller is a controller class
+ * That handles the movement for a rolling character.
+ * In fixed update the game takes input from the veritcal
+ * and horizontal axes (WASD and Arrow keys) and converts
+ * them into movement components based on the camera's angle.
+ * 
+ * It also has a function that caps the speed at a top speed
+ * 
+ * Author: Josiah Stewart
+ */
+using UnityEngine;
 using System.Collections;
 
 public class RollingPlayerController : MonoBehaviour 
 {
-	public float speed = 50;
+	public float acceleration = 50;
 	public float maxSpeed = 10;
 	public Camera ballCamera;
+
+	private float angularDragInitial;
+
+	void Start()
+	{
+		angularDragInitial = GetComponent<Rigidbody> ().angularDrag;
+	}
 
 	//Pre Physics Calculation
 	void FixedUpdate()
 	{
-		//This Block handles the movement relative to the camera
-		float moveHorizontal = Mathf.Sin (ballCamera.transform.localRotation.eulerAngles.y * Mathf.Deg2Rad) *
-			Input.GetAxis("Vertical") +
-			Mathf.Cos (ballCamera.transform.localRotation.eulerAngles.y * Mathf.Deg2Rad) *
-			Input.GetAxis("Horizontal");;
-		float moveVertical = -Mathf.Sin (ballCamera.transform.localRotation.eulerAngles.y * Mathf.Deg2Rad) *
-			Input.GetAxis("Horizontal") +
-			Mathf.Cos (ballCamera.transform.localRotation.eulerAngles.y * Mathf.Deg2Rad) *
-			Input.GetAxis("Vertical");
+		Rigidbody rb = GetComponent<Rigidbody>();
+		float cameraAngle = ballCamera.transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
 
-		//This block handles the max speed calculations
-		if (GetComponent<Rigidbody> ().velocity.x > maxSpeed &&
-			moveHorizontal > 0)
+		//Calculates amount of input that is left and right relative to the camera
+		float moveHorizontal = Mathf.Sin (cameraAngle) * Input.GetAxis("Vertical") +
+							   Mathf.Cos (cameraAngle) * Input.GetAxis("Horizontal");
+		//Calculates amount of input that is forwards and backwards relative to the camera
+		float moveVertical =  -Mathf.Sin (cameraAngle) * Input.GetAxis("Horizontal") +
+							   Mathf.Cos (cameraAngle) * Input.GetAxis("Vertical");
+		
+		if (rb.velocity.x > maxSpeed && moveHorizontal > 0)
 			moveHorizontal = 0;
-		if (GetComponent<Rigidbody> ().velocity.x * -1 > maxSpeed &&
-		    moveHorizontal < 0)
+		if (rb.velocity.x * -1 > maxSpeed && moveHorizontal < 0)
 			moveHorizontal = 0;
-		if (GetComponent<Rigidbody> ().velocity.z > maxSpeed &&
-		    moveVertical > 0)
+		if (rb.velocity.z > maxSpeed && moveVertical > 0)
 			moveVertical = 0;
-		if (GetComponent<Rigidbody> ().velocity.z * -1 > maxSpeed &&
-		    moveVertical < 0)
+		if (rb.velocity.z * -1 > maxSpeed && moveVertical < 0)
 			moveVertical = 0;
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Vector3 movement = new Vector3 (moveHorizontal * 0.85f, 0.0f, moveVertical);
 
-		GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime * 100);
+		Debug.Log (GetComponent<Rigidbody> ().angularDrag);
+		if (IsGrounded ()) 
+		{
+			GetComponent<Rigidbody> ().angularDrag = angularDragInitial;
+			GetComponent<Rigidbody> ().AddForce (movement * acceleration * Time.deltaTime * 100);
+		} else 
+		{
+			GetComponent<Rigidbody> ().angularDrag = 0;
+			GetComponent<Rigidbody> ().AddForce (movement * acceleration * Time.deltaTime * 10);
+		}
+	}
+
+	public bool IsGrounded()
+	{
+		return Physics.Raycast (GetComponent<Rigidbody>().transform.position, Vector3.down, 0.5f);
 	}
 }
--- Scripts/ThirdPersonCamFollow.cs
+++ Scripts/ThirdPersonCamFollow.cs
@@ -4,21 +4,24 @@ using System.Collections;
 public class ThirdPersonCamFollow : MonoBehaviour {
 
 	public GameObject player;
-	private Vector3 offset;
+	public float camFollow = 0.05f;
+	public Camera ballCamera;
+	public GameObject playerContainer;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		offset = transform.position;
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () 
+	void FixedUpdate () 
 	{
-		//transform.position = player.transform.position + offset;
-		if (Input.GetKey ("q"))
-			transform.RotateAround (player.transform.position, Vector3.up, 100 * Time.deltaTime);
-		if (Input.GetKey ("e"))
-			transform.RotateAround (player.transform.position, Vector3.up, -100 * Time.deltaTime);
+		//Target position = ball location - 4 * the direction of movement + 2y.
+		Vector3 targetPosition = player.transform.position -
+			player.GetComponent<Rigidbody> ().velocity.normalized *
+			3 + new Vector3 (0, 2, 0);
+		if(player.GetComponent<Rigidbody> ().velocity.magnitude > 0.25)
+			transform.position += camFollow * (targetPosition - transform.position);
+		transform.LookAt (player.transform);
 	}
 }
