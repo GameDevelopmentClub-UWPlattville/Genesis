a4b167fdcd7bd4aba7d7b029c9dc581a535553f0
unknown <sprimontb@WKS003771.ion.uwplatt.edu>
2015-04-22 20:22:50 -0500

    Added map. -sprimontb

---
+++ Scripts/Spin_Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin_Script : MonoBehaviour
+{
+	public Vector3 eulerAngleVelocity;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		//GetComponent<Rigidbody> ().AddTorque (0, 1000.0f, 0);
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		//float torque = Mathf.Sin (Time.time);
+		//transform.Rotate(new Vector3(0, 10.0f, 0));
+		//GetComponent<Rigidbody> ().AddTorque (0, 10.0f, 0);
+		//GetComponent<Rigidbody> ().rotation() > 0;
+		//Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+		//rb.MoveRotation(rb.rotation * deltaRotation);
+
+		rb.angularVelocity = (eulerAngleVelocity);
+	}
+}
